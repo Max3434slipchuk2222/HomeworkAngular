abae8bbc0f046123a0e9f6d7ad11e5a583dad9b0
Ваше Ім'я <_email@example.com>
2025-10-19 23:15:26 +0300

    Create

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Header} from './header/header';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
+  selector: 'app-salo',
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('my-angular');
+
+  public counter = 0;
+
+  increment(){
+    this.counter++;
+  }
 }
